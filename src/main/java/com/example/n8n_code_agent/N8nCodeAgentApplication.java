80ce175a409659c00a14c9da63f1dc2f032bd9ec
vikas <>
2025-12-05 09:11:23 +0700

    inital version

---
+++ src/main/java/com/example/n8n_code_agent/N8nCodeAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.n8n_code_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N8nCodeAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N8nCodeAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/n8n_code_agent/N8nCodeAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.n8n_code_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N8nCodeAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
